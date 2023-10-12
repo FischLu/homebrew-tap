69fe7fff913a37853612cff0a7191940450867b0
FischLu <bigplume@gmail.com>
2023-10-12 15:28:24 +0200

    add calendr

---
+++ Casks/calendr.rb
@@ -0,0 +1,10 @@
+cask "calendr" do
+    version "1.10.2"
+  
+    url "https://github.com/pakerwreah/Calendr/releases/download/v#{version}/Calendr.zip"
+    appcast "https://github.com/pakerwreah/Calendr/releases.atom"
+    name "Calendr.app"
+    homepage "https://github.com/pakerwreah/Calendr"
+  
+    app "Calendr.app"
+  end
